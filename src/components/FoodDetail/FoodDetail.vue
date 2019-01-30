05a0307a394778845fe2709d9d4be55ebbf5148d
lisurong <lisurong@300.cn>
2019-01-30 16:05:18 +0800

    22

---
+++ src/components/FoodDetail/FoodDetail.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="foodDetail">
+  		{{foodData1.name}}
+	</div>
+</template>
+
+<script>
+
+export default {
+  props:{
+    foodData1:{
+      type:Object
+    }
+  },
+  data(){
+      return{
+        
+  	}
+  },
+  methods:{
+  
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.foodDetail{
+  position: fixed;
+  top: 0;
+  left:0;
+  bottom: 50px;
+  background: #fff;
+  width: 100%;
+}
+</style>
